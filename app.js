c895b2f2df7ffdf033a697d8134cfff379503c23
Shubhan <ronyshv@gmail.com>
2020-05-13 12:13:43 -0400

    all done

--- app.js
+++ app.js
@@ -1,28 +1,70 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const request = require('request')
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const https = require('https');
 
-const app = express()
+const app = express();
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/signup.html')
-})
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/signup.html');
+});
 
-app.post('/', function(req, res){
+app.post('/', function (req, res) {
+	const firstName = req.body.fName;
+	const lastName = req.body.lName;
+	const email = req.body.email;
 
-const firstName = req.body.fName
-const lastName = req.body.lName
-const email = req.body.email
+	const data = {
+		members: [
+			{
+				email_address: email,
+				status: 'subscribed',
+				merge_fields: {
+					FNAME: firstName,
+					LNAME: lastName,
+				},
+			},
+		],
+	};
+	const jsonData = JSON.stringify(data);
 
-console.log(firstName,lastName,email)
+	const url = 'https://us18.api.mailchimp.com/3.0/lists/481fa17e58';
 
-})
+	const options = {
+		method: 'POST',
+		auth: 'smajumdar1:3544c0c649cd44d5e013a57ffb8c2ec5-us18',
+	};
 
-app.listen(3000, function(){
-    console.log('server running on port 3000')
-})
+	const request = https.request(url, options, function (response) {
+		if (response.statusCode === 200) {
+			res.sendFile(__dirname + '/success.html');
+		} else {
+			res.sendFile(__dirname + '/failure.html');
+		}
 
+		response.on('data', function (data) {
+			console.log(JSON.parse(data));
+		});
+	});
+	request.write(jsonData);
+	request.end();
+	// console.log(firstName, lastName, email)
+});
+
+app.post('/failure', function (req, res) {
+	res.redirect('/');
+});
+
+app.listen(process.env.PORT || 3000, function () {
+	console.log('server running on port 3000');
+});
+
+// API Keys
+// 3544c0c649cd44d5e013a57ffb8c2ec5-us18
+
+//List ID
+// 481fa17e58
